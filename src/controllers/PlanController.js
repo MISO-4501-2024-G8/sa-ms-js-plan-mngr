77976135b8108110ca5a2c734b744beb74e5014d
Juan Sebastian Sanchez L. <js.sanchezl12@uniandes.edu.co>
2024-04-16 12:54:42 -0500

    Refactor handlePlanOperation function in PlanController.js, update checkToken utility function, and fix unit tests

--- src/controllers/PlanController.js
+++ src/controllers/PlanController.js
@@ -202,4 +202,485 @@ planController.delete('/descriptionFeatures/:id', async (req, res) => {
 });
 
 
+// crear un plan basico
+planController.post('/planbasico', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de creación de plan básico');
+            checkRequest(req, 'creación');
+            console.log('Petición de creación de plan básico:', JSON.stringify(req.body));
+            const idModel = uuidv4().split('-')[0];
+            const typePlan = 'basico';
+            const { name, startDate, endDate, value } = req.body;
+            const planInfo = await Plan.create(
+                {
+                    id: idModel,
+                    name,
+                    typePlan,
+                    startDate,
+                    endDate,
+                    value
+                }
+            );
+            res.status(201).json(planInfo);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+// crear un plan basico-intermedio
+
+planController.post('/planbasico_intermedio', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de creación de plan básico-intermedio');
+            checkRequest(req, 'creación');
+            console.log('Petición de creación de plan básico-intermedio:', JSON.stringify(req.body));
+            const idModel = uuidv4().split('-')[0];
+            const typePlan = 'intermedio';
+            const { name, startDate, endDate, value, monitoreoTiempoReal, alertasRiesgo, comunicacionEntrenador } = req.body;
+            const planInfo = await Plan.create(
+                {
+                    id: idModel,
+                    name,
+                    typePlan,
+                    startDate,
+                    endDate,
+                    value
+                }
+            );
+            const planIntermedioInfo = await PlanIntermedio.create(
+                {
+                    id: idModel,
+                    monitoreoTiempoReal,
+                    alertasRiesgo,
+                    comunicacionEntrenador
+                }
+            );
+
+            const planIntermedio = {
+                planInfo,
+                planIntermedioInfo
+            };
+
+            res.status(201).json(planIntermedio);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+// crear un plan basico-premium
+
+planController.post('/planbasico_premium', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de creación de plan básico-premium');
+            checkRequest(req, 'creación');
+            console.log('Petición de creación de plan básico-premium:', JSON.stringify(req.body));
+            const idModel = uuidv4().split('-')[0];
+            const typePlan = 'premium';
+            const {
+                name, startDate, endDate, value,
+                monitoreoTiempoReal, alertasRiesgo, comunicacionEntrenador,
+                sesionesVirtuales, masajes, cuidadoPosEjercicio } = req.body;
+            const planInfo = await Plan.create(
+                {
+                    id: idModel,
+                    name,
+                    typePlan,
+                    startDate,
+                    endDate,
+                    value
+                }
+            );
+            const planIntermedioInfo = await PlanIntermedio.create(
+                {
+                    id: idModel,
+                    monitoreoTiempoReal,
+                    alertasRiesgo,
+                    comunicacionEntrenador
+                });
+
+            const planPremiumInfo = await PlanPremium.create(
+                {
+                    id: idModel,
+                    sesionesVirtuales,
+                    masajes,
+                    cuidadoPosEjercicio
+                }
+            );
+
+            const planPremium = {
+                planInfo,
+                planIntermedioInfo,
+                planPremiumInfo
+            };
+
+            res.status(201).json(planPremium);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+// actualizar un plan basico
+
+planController.put('/planbasico/:id', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de actualización de plan básico');
+            checkRequest(req, 'actualización');
+            console.log('Petición de actualización de plan básico:', JSON.stringify(req.body));
+            const plan = await findPlanById(Plan, req.params.id);
+            if (plan === null) {
+                res.status(404).json({ error: 'No se encontró el plan', code: 404 });
+            }
+            const { name, startDate, endDate, value } = req.body;
+            plan.set({ name, startDate, endDate, value });
+            await plan.save();
+            res.status(200).json(plan);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+// actualizar un plan basico-intermedio
+
+planController.put('/planbasico_intermedio/:id', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de actualización de plan básico-intermedio');
+            checkRequest(req, 'actualización');
+            console.log('Petición de actualización de plan básico-intermedio:', JSON.stringify(req.body));
+            const plan = await findPlanById(Plan, req.params.id);
+            const planIntermedio = await findPlanById(PlanIntermedio, req.params.id);
+            if (plan === null || planIntermedio === null) {
+                res.status(404).json({ error: 'No se encontró el plan intermedio', code: 404 });
+            }
+            const { name, startDate, endDate, value, monitoreoTiempoReal, alertasRiesgo, comunicacionEntrenador } = req.body;
+
+            plan.set({ name, startDate, endDate, value });
+            await plan.save();
+            planIntermedio.set({ monitoreoTiempoReal, alertasRiesgo, comunicacionEntrenador });
+            await planIntermedio.save();
+
+            const planIntermedioInfo = {
+                plan,
+                planIntermedio
+            };
+            res.status(200).json(planIntermedioInfo);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+// actualizar un plan basico-premium
+
+planController.put('/planbasico_premium/:id', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de actualización de plan básico-premium');
+            checkRequest(req, 'actualización');
+            console.log('Petición de actualización de plan básico-premium:', JSON.stringify(req.body));
+            const plan = await findPlanById(Plan, req.params.id);
+            const planIntermedio = await findPlanById(PlanIntermedio, req.params.id);
+            const planPremium = await findPlanById(PlanPremium, req.params.id);
+            if (plan === null || planIntermedio === null || planPremium === null) {
+                res.status(404).json({ error: 'No se encontró el plan premium', code: 404 });
+            }
+            const {
+                name, startDate, endDate, value,
+                monitoreoTiempoReal, alertasRiesgo, comunicacionEntrenador,
+                sesionesVirtuales, masajes, cuidadoPosEjercicio } = req.body;
+            plan.set({ name, startDate, endDate, value });
+            await plan.save();
+            planIntermedio.set({ monitoreoTiempoReal, alertasRiesgo, comunicacionEntrenador });
+            await planIntermedio.save();
+            planPremium.set({ sesionesVirtuales, masajes, cuidadoPosEjercicio });
+            await planPremium.save();
+            const planPremiumInfo = {
+                plan,
+                planIntermedio,
+                planPremium
+            };
+            res.status(200).json(planPremiumInfo);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+// eliminar un plan basico
+
+planController.delete('/planbasico/:id', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de eliminación de plan básico');
+            checkRequest(req, 'eliminación');
+            console.log('Petición de eliminación de plan básico:', JSON.stringify(req.body));
+            const plan = await findPlanById(Plan, req.params.id);
+            if (plan === null) {
+                res.status(404).json({ error: 'No se encontró el plan', code: 404 });
+            }
+            if (process.env.NODE_ENV !== 'test') {
+                await plan.destroy();
+            }
+            res.status(200).json(plan);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+// eliminar un plan basico-intermedio
+
+planController.delete('/planbasico_intermedio/:id', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de eliminación de plan básico-intermedio');
+            checkRequest(req, 'eliminación');
+            console.log('Petición de eliminación de plan básico-intermedio:', JSON.stringify(req.body));
+            const plan = await findPlanById(Plan, req.params.id);
+            const planIntermedio = await findPlanById(PlanIntermedio, req.params.id);
+            if (plan === null || planIntermedio === null) {
+                res.status(404).json({ error: 'No se encontró el plan intermedio', code: 404 });
+            }
+            if (process.env.NODE_ENV !== 'test') {
+                await plan.destroy();
+                await planIntermedio.destroy();
+            }
+            const planIntermedioInfo = {
+                plan,
+                planIntermedio
+            };
+            res.status(200).json(planIntermedioInfo);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+// eliminar un plan basico-premium
+
+planController.delete('/planbasico_premium/:id', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de eliminación de plan básico-premium');
+            checkRequest(req, 'eliminación');
+            console.log('Petición de eliminación de plan básico-premium:', JSON.stringify(req.body));
+            const plan = await findPlanById(Plan, req.params.id);
+            const planIntermedio = await findPlanById(PlanIntermedio, req.params.id);
+            const planPremium = await findPlanById(PlanPremium, req.params.id);
+            if (plan === null || planIntermedio === null || planPremium === null) {
+                res.status(404).json({ error: 'No se encontró el plan premium', code: 404 });
+            }
+            if (process.env.NODE_ENV !== 'test') {
+                await plan.destroy();
+                await planIntermedio.destroy();
+                await planPremium.destroy();
+            }
+            const planPremiumInfo = {
+                plan,
+                planIntermedio,
+                planPremium
+            };
+            res.status(200).json(planPremiumInfo);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+const createPlanFeature = async (req, tipoPlan) => {
+
+    return descriptionFeature;
+};
+
+// agregar una caracteristica de descripcion a plan
+planController.post('/feature', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de creación de característica de descripción');
+            checkRequest(req, 'creación');
+            console.log('Petición de creación de característica de descripción:', JSON.stringify(req.body));
+            const idModel = uuidv4().split('-')[0];
+            const { tipoPlan, description } = req.body;
+            const planFeature = await DescriptionFeatures.create(
+                {
+                    id: idModel,
+                    tipoPlan,
+                    description
+                }
+            );
+            res.status(201).json(planFeature);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+// actualizar una caracteristica de descripcion a plan
+planController.put('/feature/:id', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de actualización de característica de descripción');
+            checkRequest(req, 'actualización');
+            console.log('Petición de actualización de característica de descripción:', JSON.stringify(req.body));
+            const planFeature = await findPlanById(DescriptionFeatures, req.params.id);
+            if (planFeature === null) {
+                res.status(404).json({ error: 'No se encontró la característica de descripción', code: 404 });
+            }
+            const { tipoPlan, description } = req.body;
+            planFeature.set({ tipoPlan, description });
+            await planFeature.save();
+            res.status(200).json(planFeature);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+// eliminar una caracteristica de descripcion a plan
+planController.delete('/feature/:id', async (req, res) => {
+    try {
+        const isToken = await checkToken(req, res);
+        if (isToken === '') {
+            console.log('Petición de eliminación de característica de descripción');
+            checkRequest(req, 'eliminación');
+            console.log('Petición de eliminación de característica de descripción:', JSON.stringify(req.body));
+            const planFeature = await findPlanById(DescriptionFeatures, req.params.id);
+            if (planFeature === null) {
+                res.status(404).json({ error: 'No se encontró la característica de descripción', code: 404 });
+            }
+            if (process.env.NODE_ENV !== 'test') {
+                await planFeature.destroy();
+            }
+            res.status(200).json(planFeature);
+        } else {
+            res.status(401).json({ error: isToken, code: 401 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+
+const getPlanFeatures = async (tipoPlan) => {
+    const descriptionFeatures = await DescriptionFeatures.findAll({ where: { tipoPlan: tipoPlan } });
+    return descriptionFeatures;
+};
+
+// obtener todas las caracteristicas de descripcion de un plan
+planController.get('/features/:tipoPlan', async (req, res) => {
+    try {
+        console.log('Petición de obtener todas las características de descripción de un plan');
+        const features = getPlanFeatures(req.params.tipoPlan);
+        res.status(200).json(features);
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+
+const getPlanInfo = async (plan) => {
+    if (plan.typePlan === 'basico') {
+        // No se necesita agregar información adicional para el plan básico
+    } else if (plan.typePlan === 'intermedio') {
+        // Buscar información del plan intermedio y agregarla al objeto de plan
+        const planIntermedio = await PlanIntermedio.findOne({ where: { planId: plan.id } });
+        if (planIntermedio) {
+            plan.intermedioInfo = planIntermedio.info;
+        }
+    } else if (plan.typePlan === 'premium') {
+        // Buscar información del plan intermedio y agregarla al objeto de plan
+        const planIntermedio = await PlanIntermedio.findOne({ where: { planId: plan.id } });
+        if (planIntermedio) {
+            plan.intermedioInfo = planIntermedio.info;
+        }
+
+        // Buscar información del plan premium y agregarla al objeto de plan
+        const planPremium = await PlanPremium.findOne({ where: { planId: plan.id } });
+        if (planPremium) {
+            plan.premiumInfo = planPremium.info;
+        }
+    }
+    // Buscar características adicionales y agregarlas al objeto de plan
+    const descriptionFeatures = getPlanFeatures(plan.typePlan);
+    plan.features = descriptionFeatures.map(feature => feature.feature);
+
+    return plan;
+}
+
+// obtener todos los planes
+
+planController.get('/allplans', async (req, res) => {
+    try {
+        console.log('Petición de obtener todos los planes');
+        const planes = await Plan.findAll();
+        const planesInfo = [];
+        for (let plan of planes) {
+            planesInfo.push(getPlanInfo(plan));
+        }
+        res.status(200).json(planesInfo);
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+// obtener un plan por id
+planController.get('/allplan/:id', async (req, res) => {
+    try {
+        console.log('Petición de obtener un plan por id');
+        const plan = await Plan.findOne({ where: { id: req.params.id } });
+        if (plan) {
+            const planInfo = await getPlanInfo(plan);
+            res.status(200).json(planInfo);
+        } else {
+            res.status(404).json({ error: 'No se encontró el plan', code: 404 });
+        }
+    } catch (error) {
+        res.status(500).json(errorHandling(error));
+    }
+});
+
+
+
+
+
 module.exports = planController;
